b688f756cad8f3e43c3862d0b4291d86b2c89928
Solomon David <solomonjohnson.david@havells.com>
2024-01-27 23:46:05 +0530

    renamed variables

--- createConnection/getMongoCollection.js
+++ createConnection/getMongoCollection.js
@@ -1,9 +1,9 @@
 const singletonConnection = require("./singletonConnection");
 const config = require("../config/config");
 
-module.exports = async (collectionName) => {
+module.exports = async (collectionId) => {
     const mongoClient = await singletonConnection.createMongoConnection();
     const database =  mongoClient.db(config.db.mongo.databaseId);
-    const collection =  database.collection(collectionName);
+    const collection =  database.collection(collectionId);
     return collection;
 }
